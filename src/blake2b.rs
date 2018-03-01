7a5b5fc99ae483a0043db7547fb79a6fa44b88a9
George Tankersley <george.tankersley@gmail.com>
2018-03-01 00:00:00 +0000

    blake2: add support for salting and personalization

--- src/blake2b.rs
+++ src/blake2b.rs
@@ -36,7 +36,9 @@
 
 #![cfg_attr(feature = "cargo-clippy", allow(unreadable_literal))]
 
-blake2_impl!(Blake2b, Blake2bResult, blake2b, u64, u64x4, 64, 32, 24, 16, 63, [
+blake2_impl!(
+    Blake2b, Blake2bResult, blake2b, u64,
+    u64x4, read_u64, 64, 32, 24, 16, 63, [
     0x6A09E667F3BCC908, 0xBB67AE8584CAA73B,
     0x3C6EF372FE94F82B, 0xA54FF53A5F1D36F1,
     0x510E527FADE682D1, 0x9B05688C2B3E6C1F,
